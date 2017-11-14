2bd8553ca14ab83b74ed9fdd481e9284f7411e84
Arcnet <bcscarney@gmail.com>
2017-11-14 15:18:45 -0600

    update

---
+++ android/src/com/jump/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jump.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jump.game.JumpGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JumpGame(), config);
+	}
+}
